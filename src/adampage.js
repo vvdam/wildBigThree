6d2c9fad0eec66b52a66a1dd6428b0930546d415
Anthony DUFRENOT <anthony.dufrenot@gmail.com>
2024-03-13 17:08:03 +0100

    fin de journée enregistrement des dernières modifs

--- src/adampage.js
+++ src/adampage.js
@@ -11,3 +11,35 @@ closeButton.addEventListener('click', () => {
     navBar.classList.remove('open');
     burgerButton.classList.remove('remove');
 })
+
+const movieTitles = document.querySelectorAll(".mainimage");
+
+movieTitles.forEach(el => {
+    el.addEventListener("mousemove", e => {
+
+        let elRect = el.getBoundingClientRect()
+
+        let x = e.clientX - elRect.x
+        let y = e.clientY - elRect.y
+
+
+        let midCardWidth = elRect.width / 2
+        let midCardHeight = elRect.height / 2
+
+
+
+
+        let angleY = (x - midCardWidth) / 4
+        let angleX = (y - midCardHeight) / 4
+
+
+        el.style.transform = `perspective(900px) rotateX(${angleX}deg) rotateY(${angleY}deg) scale(1.1)`
+
+    })
+    el.addEventListener("mouseleave", e => {
+
+        el.style.transform = 'none'
+
+    })
+
+});
